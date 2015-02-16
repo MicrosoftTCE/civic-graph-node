b90ac987c919d988d4d4c37d7aa7357b449fcdfb
Ken Chan <kchan923@gmail.com>
2015-02-16 15:13:35 -0500

    adding localhost capabilities to enable developer participation

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ var logger = require('./logger');
 app.use(logger);
 
 app.use(connection(mysql, {
-    host: 'us-cdbr-azure-east-b.cloudapp.net',
-    user: 'b5a370a8f6d91a',
-    password: 'e928dad7',
+    host: 'localhost',
+    user: 'root',
+    password: 'MicrosoftNY',
     port: 3306,
     database: 'athena'
 }, 'request'));
@@ -50,23 +50,23 @@ app.use(connection(mysql, {
 // });
 app.set('json spaces', 20);
 
-var athena = require('./routes/athena');
-app.get('/athena', athena.retrieve_all);
+// var athena = require('./routes/athena');
+// app.get('/athena', athena.retrieve_all);
 
-var entities = require('./routes/entities'); 
-app.get('/entities', entities.retrieve_entities);
-app.get('/entities/:name', entities.retrieve_entity);
-app.get('/entities/:name/categories', entities.retrieve_categories);
-app.get('/entities/:name/key_people', entities.retrieve_key_people);
-app.get('/entities/:name/relations', entities.retrieve_relations);
-app.get('/entities/:name/funding_received', entities.retrieve_funding_received);
-app.get('/entities/:name/funding_given', entities.retrieve_funding_given);
-app.get('/entities/:name/investments_received', entities.retrieve_investments_received);
-app.get('/entities/:name/investments_made', entities.retrieve_investments_made);
-app.get('/entities/:name/collaborations', entities.retrieve_collaborations);
-app.get('/entities/:name/data', entities.retrieve_data);
-app.get('/entities/:name/revenue', entities.retrieve_revenue);
-app.get('/entities/:name/expenses', entities.retrieve_expenses);
+// var entities = require('./routes/entities'); 
+// app.get('/entities', entities.retrieve_entities);
+// app.get('/entities/:name', entities.retrieve_entity);
+// app.get('/entities/:name/categories', entities.retrieve_categories);
+// app.get('/entities/:name/key_people', entities.retrieve_key_people);
+// app.get('/entities/:name/relations', entities.retrieve_relations);
+// app.get('/entities/:name/funding_received', entities.retrieve_funding_received);
+// app.get('/entities/:name/funding_given', entities.retrieve_funding_given);
+// app.get('/entities/:name/investments_received', entities.retrieve_investments_received);
+// app.get('/entities/:name/investments_made', entities.retrieve_investments_made);
+// app.get('/entities/:name/collaborations', entities.retrieve_collaborations);
+// app.get('/entities/:name/data', entities.retrieve_data);
+// app.get('/entities/:name/revenue', entities.retrieve_revenue);
+// app.get('/entities/:name/expenses', entities.retrieve_expenses);
 
 // var connections = require('./routes/connections');
 // app.get('/connections', connections.retrieve_connections);
@@ -81,8 +81,8 @@ app.get('/entities/:name/expenses', entities.retrieve_expenses);
 // app.get('/operations/revenue/year', operations.retrieve_revenue_year);
 // app.get('/operations/expenses/year', operations.retrieve_expenses_year);
 
-var database = require('./routes/database');
-app.post('/database/save', parseUrlencoded, database.save);
+// var database = require('./routes/database');
+// app.post('/database/save', parseUrlencoded, database.save);
 
 // app.get('/', function(req, res) {
 //   res.sendFile(__dirname + '/index.html');
--- routes/connections.js
+++ routes/connections.js
@@ -1,10 +1,54 @@
 var fs = require('fs');
 var file = __dirname + '../../public/data/civic.json';
 
-// var getConnectionData = function(identifier, request){
-  
+// var connections = require('./routes/connections');
+// app.get('/connections', connections.retrieve_connections);
+// app.get('/connections/:name', connections.retrieve_entity_connections);
+// app.get('/connections/:name/funding', connections.retrieve_entity_funding);
+// app.get('/connections/:name/investments', connections.retrieve_entity_investments);
+// app.get('/connections/:name/collaborations', connections.retrieve_entity_collaborations);
+// app.get('/connections/:name/data', connections.retrieve_entity_data);
+
+exports.retrieve_connections = function(request, response){
+  request.getConnection(function (err, connection) {
+    if (err) throw err;
+    connection.query('SELECT Entities.name AS source_name, Entity1ID AS source, Entity2ID AS target, Connection AS connection, Amount AS amount, ConnectionYear AS year FROM Connections INNER JOIN Entities ON Connections.Entity1ID = Entities.ID', function(err, rows){
+      response.json(rows);
+    });
+  });
+};
+
+// exports.retrieve_entity_connections = function(request, response){
+//   request.getConnection(function (err, connection) {
+//     if (err) throw err;
+//     request.params.name
+
+//     connection.query('SELECT Entity1ID AS source, Entity2ID AS target, Connection AS connection, Amount AS amount, ConnectionYear AS year FROM Connections WHERE Entity1ID = ', function(err, rows){
+
+//     });
+//   });
 // });
 
-// exports.retrieve_connections = function(request, response){
-  
+// exports.retrieve_entity_funding = function(request, response){
+//   request.getConnection(function (err, connection) {
+
+//   });
+// });
+
+// exports.retrieve_entity_investments = function(request, response){
+//   request.getConnection(function (err, connection) {
+
+//   });
+// });
+
+// exports.retrieve_collaborations = function(request, response){
+//   request.getConnection(function (err, connection) {
+
+//   });
+// });
+
+// exports.retrieve_entity_data = function(request, response){
+//   request.getConnection(function (err, connection) {
+
+//   });
 // });
