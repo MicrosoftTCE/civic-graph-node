a389dad8f683ffb9040205222f15b47aff97c541
Ken Chan <kchan923@gmail.com>
2015-04-06 04:27:29 -0400

    modified several files

--- configuration/credentials.js
+++ configuration/credentials.js
@@ -1,16 +1,16 @@
 exports.cred = {
     'cleardb' : {
-      port: ****,
-      host: '**************',
-      user: '**************',
-      password: '**************',
-      database: '**************'
+      port: 3306,
+      host: 'us-cdbr-azure-east-b.cloudapp.net',
+      user: 'b184e9dc5c53b9',
+      password: 'cb023509',
+      database: 'civicteA9MEi6nPl'
     },
     'localhost' : {
-      port: ****,
-      host: '**************',
-      user: '**************',
-      password: '**************',
-      database: '**************'
+      port: 3306,
+      host: 'us-cdbr-azure-east-b.cloudapp.net',
+      user: 'b184e9dc5c53b9',
+      password: 'cb023509',
+      database: 'civicteA9MEi6nPl'
     }
 };
